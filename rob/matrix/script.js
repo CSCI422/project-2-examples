dc3d33b9ca697e1c9adbc9b55fe0df049d92abd2
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-03-17 09:11:39 -0600

    add alt text

--- rob/matrix/script.js
+++ rob/matrix/script.js
@@ -3,6 +3,7 @@ const experienceButton = document.getElementById("experience-link");
 experienceButton.addEventListener("click", () => {
   document.getElementById("home").style.display = "none";
   document.getElementById("experience").style.display = "block";
+  document.getElementById("page-header").innerHTML = "[cv]";
 });
 
 const homeButton = document.getElementById("home-link");
@@ -10,4 +11,5 @@ const homeButton = document.getElementById("home-link");
 homeButton.addEventListener("click", () => {
   document.getElementById("experience").style.display = "none";
   document.getElementById("home").style.display = "block";
+  document.getElementById("page-header").innerHTML = "[home]";
 });
